bba936eaf6da549afc1431f0d1ecc041d1e227f8
Assaf Gordon <assafgordon@gmail.com>
2013-04-05 18:26:35 -0400

    calc: initial testing framework
    run with:  make check
    or: make check VERBOSE=yes
    or: make check VERBOSE=yes DEBUG=yes SAVE_TEMPS=yes

---
+++ tests/calc-tests.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+# Until a better way comes along to auto-use Coreutils Perl modules
+# as in the coreutils' autotools system.
+use Coreutils;
+use CuSkip;
+use CuTmpdir qw(calc);
+
+(my $program_name = $0) =~ s|.*/||;
+my $prog = 'calc';
+
+# TODO: add localization tests with "grouping"
+# Turn off localization of executable's output.
+@ENV{qw(LANGUAGE LANG LC_ALL)} = ('C') x 3;
+
+# note: '5' appears twice
+my $in1 = "1\n2\n3\n4\n5\n6\n7\n5\n8\n9\n10\n";
+
+my @Tests =
+(
+  # Basic tests, single field, single group, default everything
+  ['b1', 'count 1' ,    {IN_PIPE=>$in1},  {OUT => "11\n"}],
+  ['b2', 'sum 1',       {IN_PIPE=>$in1},  {OUT => "60\n"}],
+  ['b3', 'min 1',       {IN_PIPE=>$in1},  {OUT => "1\n"}],
+  ['b4', 'max 1',       {IN_PIPE=>$in1},  {OUT => "10\n"}],
+  ['b5', 'absmin 1',    {IN_PIPE=>$in1},  {OUT => "1\n"}],
+  ['b6', 'absmax 1',    {IN_PIPE=>$in1},  {OUT => "10\n"}],
+  ['b8', 'median 1',    {IN_PIPE=>$in1},  {OUT => "5\n"}],
+  ['b9', 'mode 1',      {IN_PIPE=>$in1},  {OUT => "5\n"}],
+  ['b10', 'antimode 1', {IN_PIPE=>$in1},  {OUT => "1\n"}],
+  ['b11', 'unique 1',   {IN_PIPE=>$in1},  {OUT => "1,10,2,3,4,5,6,7,8,9\n"}],
+  ['b12', 'uniquenc 1', {IN_PIPE=>$in1},  {OUT => "1,10,2,3,4,5,6,7,8,9\n"}],
+  ['b13', 'collapse 1', {IN_PIPE=>$in1},  {OUT => "1,2,3,4,5,6,7,5,8,9,10\n"}],
+
+  # on a different architecture, would printf(%Lg) print something else?
+  # Use OUT_SUBST to trim output to 1.3 digits
+  ['b14', 'mean 1',     {IN_PIPE=>$in1},  {OUT => "5.454\n"},
+	  {OUT_SUBST=>'s/^(\d\.\d{3}).*/\1/'}],
+  ['b15', 'pstdev 1',   {IN_PIPE=>$in1},  {OUT => "2.742\n"},
+	  {OUT_SUBST=>'s/^(\d\.\d{3}).*/\1/'}],
+  ['b16', 'sstdev 1',   {IN_PIPE=>$in1},  {OUT => "2.876\n"},
+	  {OUT_SUBST=>'s/^(\d\.\d{3}).*/\1/'}],
+  ['b17', 'pvar 1',     {IN_PIPE=>$in1},  {OUT => "7.520\n"},
+	  {OUT_SUBST=>'s/^(\d\.\d{3}).*/\1/'}],
+  ['b18', 'svar 1',     {IN_PIPE=>$in1},  {OUT => "8.272\n"},
+	  {OUT_SUBST=>'s/^(\d\.\d{3}).*/\1/'}],
+
+);
+
+my $save_temps = $ENV{SAVE_TEMPS};
+my $verbose = $ENV{VERBOSE};
+
+my $fail = run_tests ($program_name, $prog, \@Tests, $save_temps, $verbose);
+exit $fail;
