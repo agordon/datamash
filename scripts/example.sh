0072150b2654c26058c057eff71e56fcfc0a1922
Assaf Gordon <assafgordon@gmail.com>
2013-04-09 18:32:30 -0400

    cleanups: remove 'scripts' directory
    There are no add-on scripts for calc (at the moment).

--- scripts/example.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-##
-## This is an example script - installed in the "bin" directory
-## together with the main compiled program.
-##
-
-echo "Hello (example script) World"
--- scripts/support.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-##
-## This is a "support" script,
-## will be installed in the "/usr/share/APP/script" directory,
-##
-echo "Hello (support script) World"
