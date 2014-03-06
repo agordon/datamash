92cfe4c48b992b2297fbccc93afe79d08076fafb
A. Gordon <agordon@wi.mit.edu>
2014-03-05 21:13:11 -0500

    tests: add valgrind tests

---
+++ tests/calc-valgrind.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+. "./init.sh"; path_prepend_ ./src
+
+## Ensure valgrind is useable
+## (copied from coreutils' init.cfg)
+valgrind --error-exitcode=1 true 2>/dev/null ||
+    skip_ "requires a working valgrind"
+
+fail=0
+
+seq 10000 | valgrind --leak-check=full --error-exitcode=1 \
+                 calc unique 1 > /dev/null || { warn_ "unique 1 - failed" ; fail=1 ; }
+
+seq 10000 | sed 's/^/group /' |
+     valgrind --leak-check=full --error-exitcode=1 \
+                 calc -g 1 unique 1 > /dev/null || { warn_ "-g 1 unique 1 - failed" ; fail=1 ; }
+
+seq 10000 | valgrind --leak-check=full --error-exitcode=1 \
+                 calc countunique 1 > /dev/null || { warn_ "countunique 1 - failed" ; fail=1 ; }
+
+seq 10000 | valgrind --leak-check=full --error-exitcode=1 \
+                 calc collapse 1 > /dev/null || { warn_ "collapse 1 - failed" ; fail=1 ; }
+
+Exit $fail
+
