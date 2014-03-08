69c886ecec66b0a022413cfd8b4582e0d6c402d9
A. Gordon <agordon@wi.mit.edu>
2014-03-07 21:48:44 -0500

    maint: update copyright year, add GPL

--- tests/calc-tests.pl
+++ tests/calc-tests.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 =pod
   Unit Tests for calc - perform simple calculation on input data
-  Copyright (C) 2013 Assaf Gordon.
+  Copyright (C) 2013-2014 Assaf Gordon.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
